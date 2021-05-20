3b17941ec0397adc52325f484d015ff066e6e111
Junmo Lee <ljun4121@gmail.com>
2021-05-20 15:34:01 +0900

    Add few more boxes

--- testbed/tests/junmo.cpp
+++ testbed/tests/junmo.cpp
@@ -28,7 +28,8 @@ public:
 
 	JMScene()
 	{
-		m_world->SetGravity(b2Vec2(0.0f,-9.8f));
+		float density = 100.0f;
+		m_world->SetGravity(b2Vec2(0.0f,-1.0f));
 		{
 			b2PolygonShape shape;
 			shape.SetAsBox(5.5f, 1.5f);
@@ -37,24 +38,37 @@ public:
 			bd.position.Set(0.0f,0.0f);
 			bd.bullet = true;
 			b2Body* body = m_world->CreateBody(&bd);
-			body->CreateFixture(&shape, 1.0f);
+			body->CreateFixture(&shape, density);
 		}
 
 		{
 			b2PolygonShape shape;
-			// b2EdgeShape shape;
-			// shape.SetTwoSided(b2Vec2(-20.0f, 0.0f), b2Vec2(20.0f, 0.0f));
-			float length_triangle = 2.0f;
-			
-			const b2Vec2 points[] = {b2Vec2(-length_triangle / 2.0f, 0.0f), b2Vec2(length_triangle / 2.0f, 0.0f), b2Vec2(0.0f, length_triangle*0.866f)};
-			shape.Set(points, 3);
+			const b2Vec2 points[] = {b2Vec2(-1.0f, -1.0f), b2Vec2(1.0f, 1.0f), b2Vec2(-1.0f, 1.0f), b2Vec2(1.0f, -1.0f)};
+			shape.Set(points, 4);
+
 			for (int i = 0; i < 3; i++) {
 				b2BodyDef bd;
 				bd.type = b2_dynamicBody;
-				bd.position.Set(0.0f, (float)i * 5.0f);
+				bd.position.Set(0.0f, (float)i * 2.5f + 3.0f);
 				bd.bullet = true;
 				b2Body* body = m_world->CreateBody(&bd);
-				body->CreateFixture(&shape, 1.0f);
+				body->CreateFixture(&shape, density);
+			}
+		}
+
+		{
+			b2PolygonShape shape;
+			float length_triangle = 2.0f;
+			const b2Vec2 points[] = {b2Vec2(-length_triangle / 2.0f, 0.0f), b2Vec2(length_triangle / 2.0f, 0.0f), b2Vec2(0.0f, length_triangle*0.866f)};
+			shape.Set(points, 3);
+
+			for (int i = 0; i < 3; i++) {
+				b2BodyDef bd;
+				bd.type = b2_dynamicBody;
+				bd.position.Set(0.0f, (float)i * 5.0f + 3.0f);
+				bd.bullet = true;
+				b2Body* body = m_world->CreateBody(&bd);
+				body->CreateFixture(&shape, density);
 			}
 		}
 
